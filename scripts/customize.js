e4bdbb01aa630e1857dee4bfc969e278cc0d5823
max <maximilianwu0828@gmail.com>
2021-05-10 16:56:35 -0700

    updated customize.js again with JSON

--- scripts/customize.js
+++ scripts/customize.js
@@ -7,71 +7,91 @@ let hairImage = document.getElementById('hairImage');
 let skinImage = document.getElementById('skinImage');
 let suitImage = document.getElementById('suitImage');
 
+let hairAssets = {
+    0 : "./assets/hair0.png",
+    1 : "./assets/hair1.png",
+    2 : "./assets/hair2.png",
+};
+let hairAssetsLength = Object.keys(hairAssets).length - 1;
+
+let skinAssets = {
+    0 : "./assets/skin0.png",
+    1 : "./assets/skin1.png",
+    2 : "./assets/skin2.png",
+};
+let skinAssetsLength = Object.keys(skinAssets).length - 1;
+
+let suitAssets = {
+    0 : "./assets/suit0.png",
+    1 : "./assets/suit1.png",
+    2 : "./assets/suit2.png",
+};
+let suitAssetsLength = Object.keys(suitAssets).length - 1;
 
 function hairPrev() {
     "use strict";
     if (0 === hairNum) {
-        hairNum = 2;
+        hairNum = hairAssetsLength;
     }
     else {
         hairNum -= 1;
     }
-    hairImage.setAttribute("src", "./assets/hair" + hairNum + ".png");
+    hairImage.setAttribute("src", hairAssets[hairNum]);
 }
 
 function hairNext() {
     "use strict";
-    if (2 === hairNum) {
+    if (hairAssetsLength === hairNum) {
         hairNum = 0;
     }
     else {
         hairNum += 1;
     }
-    hairImage.setAttribute("src", "./assets/hair" + hairNum + ".png");
+    hairImage.setAttribute("src", hairAssets[hairNum]);
 }
 
 function skinPrev() {
     "use strict";
     if (0 === skinNum) {
-        skinNum = 2;
+        skinNum = skinAssetsLength;
     }
     else {
         skinNum -= 1;
     }
-    skinImage.setAttribute("src", "./assets/skin" + skinNum + ".png");
+    skinImage.setAttribute("src", skinAssets[skinNum]);
 }
 
 function skinNext() {
     "use strict";
-    if (2 === skinNum) {
+    if (skinAssetsLength === skinNum) {
         skinNum = 0;
     }
     else {
         skinNum += 1;
     }
-    skinImage.setAttribute("src", "./assets/skin" + skinNum + ".png");
+    skinImage.setAttribute("src", skinAssets[skinNum]);
 }
 
 function suitPrev() {
     "use strict";
     if (0 === suitNum) {
-        suitNum = 2;
+        suitNum = suitAssetsLength;
     }
     else {
         suitNum -= 1;
     }
-    suitImage.setAttribute("src", "./assets/suit" + suitNum + ".png");
+    suitImage.setAttribute("src", suitAssets[suitNum]);
 }
 
 function suitNext() {
     "use strict";
-    if (2 === suitNum) {
+    if (suitAssetsLength === suitNum) {
         suitNum = 0;
     }
     else {
         suitNum += 1;
     }
-    suitImage.setAttribute("src", "./assets/suit" + suitNum + ".png");
+    suitImage.setAttribute("src", suitAssets[suitNum]);
 }
 
 document.getElementById('save-button').addEventListener('click', function () {
